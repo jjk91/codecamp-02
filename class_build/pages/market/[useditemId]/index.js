ec1fcfef6aedceb8f12bff0605e2a65951e270b5
junku ji <doorly01@gmail.com>
2021-08-24 12:11:02 +0900

    tt

--- class_build/pages/market/[useditemId]/index.js
+++ class_build/pages/market/[useditemId]/index.js
@@ -1,42 +1,42 @@
-// import { request, gql } from "graphql-request";
-// import Head from "next/head";
+import { request, gql } from "graphql-request";
+import Head from "next/head";
 
-// const FETCH_USEDITEM = gql`
-//   query fetchUseditem($useditemId: ID!) {
-//     fetchUseditem(useditemId: $useditemId) {
-//       name
-//       remarks
-//       images
-//     }
-//   }
-// `;
+const FETCH_USEDITEM = gql`
+  query fetchUseditem($useditemId: ID!) {
+    fetchUseditem(useditemId: $useditemId) {
+      name
+      remarks
+      images
+    }
+  }
+`;
 
 export default function MarketPage(props) {
   return (
     <>
-      {/* <Head>
+      <Head>
         <meta property="og:title" conten={props.fetchUseditem.name} />
         <meta property="og:description" content={props.fetchUseditem.remarks} />
         <meta
           property="og:image"
           content={`https://storage.googleapis.com/${props.fetchUseditem.images[0]}`}
         />
-      </Head> */}
+      </Head>
       <div></div>
     </>
   );
 }
 
-// export const getServerSideProps = async (context) => {
-//   const result = await request(
-//     "https://backend02.codebootcamp.co.kr/graphql",
-//     FETCH_USEDITEM,
-//     {
-//       useditemId: context.query.useditemId,
-//     }
-//   );
-//   return {
-//     // 반드시 필요
-//     props: { fetchUseditem: result.fetchUseditem },
-//   };
-// };
+export const getServerSideProps = async (context) => {
+  const result = await request(
+    "https://backend02.codebootcamp.co.kr/graphql",
+    FETCH_USEDITEM,
+    {
+      useditemId: context.query.useditemId,
+    }
+  );
+  return {
+    // 반드시 필요
+    props: { fetchUseditem: result.fetchUseditem },
+  };
+};
