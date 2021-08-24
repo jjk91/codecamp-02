1c0bbb9ac25b20bfec7f8b11c49ec5d5c2686114
junku ji <doorly01@gmail.com>
2021-08-24 11:28:18 +0900

    fix1

--- class_build/pages/market/[useditemId]/index.js
+++ class_build/pages/market/[useditemId]/index.js
@@ -1,3 +1,21 @@
+import { request, gql } from "graphql-request";
+
+const FETCH_USEDITEM = gql`
+  query fetchUseditem($useditemId: ID!) {
+    fetchUseditem(useditemId: $useditemId) {
+      name
+      remarks
+      images
+    }
+  }
+`;
+
 export default function MarketPage() {
   return <div></div>;
 }
+
+export const getServerSideProps = (context) => {
+  request("https://backend02.codebootcamp.co.kr/graphql", FETCH_USEDITEM, {
+    useditemId: context.query.useditemId,
+  });
+};
